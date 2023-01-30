7f353ab98a6b8d24887ac850232adb0db9e9e69c
sebstawiarski <seb.stawiarski@outlook.com>
2023-01-30 15:01:43 +0100

    poprawienie błędnie wykomentowanego kodu w Tablica.java

--- src/Tablica.java
+++ src/Tablica.java
@@ -13,15 +13,16 @@ public class Tablica {
         System.out.println(imiona[2]);
         System.out.println(imiona[3]);*/
 
-        /*int[] lottoNumbers = new int[] {1,2,3,4,5,6};
-        System.out.println(lottoNumbers[0]);
+        int[] lottoNumbers = new int[] {1,2,3,4,5,6};
+
+        /*System.out.println(lottoNumbers[0]);
         System.out.println(lottoNumbers[1]);
         System.out.println(lottoNumbers[2]);
         System.out.println(lottoNumbers[3]);
         System.out.println(lottoNumbers[4]);
-        System.out.println(lottoNumbers[5]);
+        System.out.println(lottoNumbers[5]);*/
 
-        System.out.println(lottoNumbers.length);*/
+        System.out.println(lottoNumbers.length);
 
        for (int i=0; i<lottoNumbers.length; i++) {
             System.out.println(lottoNumbers[i]);
